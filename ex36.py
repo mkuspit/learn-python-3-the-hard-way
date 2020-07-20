2a181c1ff8262bf8f94679a44e2d440c08cbeece
Michal Kuspit <michal.kuspit@gmail.com>
2020-07-20 17:43:10 +0400

    Add exercise #36

---
+++ ex36.py
@@ -0,0 +1,33 @@
+animals = ['bear', 'pyhon3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1.
+animals[1]
+# pyhon3.6
+
+# 2. The third (3rd) animal.
+animals[2]
+# peacock
+
+# 3. The first (1st) animal.
+animals[0]
+# bear
+
+# 4. The animal at 3.
+animals[3]
+# kangaroo
+
+# 5. The fifth (5th) animal.
+animals[4]
+# whale
+
+# 6. The animal at 2.
+animals[2]
+# peacock
+
+# 7. The sixth (6th) animal.
+animals[5]
+# platypus
+
+# 8. The animal at 4.
+animals[4]
+# kangaroo
